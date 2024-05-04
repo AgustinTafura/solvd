4ace3c3afffcd92d7166ff77a8fb6246de3dcfcf
AgustinTafura <agustintafura@hotmail.com>
2024-05-04 17:23:13 -0300

    fixed task1 - hw5

--- hw_5.js
+++ hw_5.js
@@ -2,13 +2,21 @@
 function customFilterUnique(array, callback) {
     const uniqueArray = [];
     array.forEach(item => {
-        if (!uniqueArray.some(uniqueItem => callback(uniqueItem, item))) {
+        var filteredItem = callback(item)
+        const found = uniqueArray.some(uniqItem => {
+            if (typeof uniqItem === 'object') {
+                return JSON.stringify(uniqItem) === JSON.stringify(item);
+            } else {
+                return uniqItem === item;
+            }
+        });
+        if (filteredItem && !found) {
             uniqueArray.push(item);
         }
     });
-
     return uniqueArray;
 }
+
 
 const objects = [
     { id: 1, name: 'John' },
@@ -17,14 +25,14 @@ const objects = [
     { id: 3, name: 'Alice' }
 ];
 
-const callbackById = (a, b) => a.id === b.id;
-const callbackByName = (a, b) => a.name === b.name;
+// const callbackById = (a, b) => a.id === b.id;
+// const callbackByName = (a, b) => a.name === b.name;
 
-const uniqueObjectsById = customFilterUnique(objects, callbackById);
-console.log("Unique objects by ID:", uniqueObjectsById);
+// const uniqueObjectsById = customFilterUnique(objects, callbackById);
+// console.log("Unique objects by ID:", uniqueObjectsById);
 
-const uniqueObjectsByName = customFilterUnique(objects, callbackByName);
-console.log("Unique objects by Name:", uniqueObjectsByName);
+// const uniqueObjectsByName = customFilterUnique(objects, callbackByName);
+// console.log("Unique objects by Name:", uniqueObjectsByName);
 
 // Task 2:
 function chunkArray(array, chunkSize) {
@@ -93,10 +101,10 @@ function measureArrayPerformance(func, ...args) {
 const array = Array.from({ length: 100000 }, (_, index) => index + 1); 
 
 
-const customFilterUniqueByIdPerformance = measureArrayPerformance(customFilterUnique, objects, callbackById);
-const customFilterUniqueByNamePerformance = measureArrayPerformance(customFilterUnique, objects, callbackByName);
-console.log("CustomFilterUnique by ID Performance:", customFilterUniqueByIdPerformance, "milliseconds");
-console.log("CustomFilterUnique by Name Performance:", customFilterUniqueByNamePerformance, "milliseconds");
+// const customFilterUniqueByIdPerformance = measureArrayPerformance(customFilterUnique, objects, callbackById);
+// const customFilterUniqueByNamePerformance = measureArrayPerformance(customFilterUnique, objects, callbackByName);
+// console.log("CustomFilterUnique by ID Performance:", customFilterUniqueByIdPerformance, "milliseconds");
+// console.log("CustomFilterUnique by Name Performance:", customFilterUniqueByNamePerformance, "milliseconds");
 
 const chunkArrayPerformance = measureArrayPerformance(chunkArray, array, chunkSize);
 console.log("ChunkArray Performance:", chunkArrayPerformance, "milliseconds");
@@ -110,3 +118,23 @@ const getArrayIntersectionPerformance = measureArrayPerformance(getArrayIntersec
 const getArrayUnionPerformance = measureArrayPerformance(getArrayUnion, array1, array2);
 console.log("getArrayIntersection Performance:", getArrayIntersectionPerformance, "milliseconds");
 console.log("getArrayUnion Performance:", getArrayUnionPerformance, "milliseconds");
+
+
+
+//testing Task 1
+console.log('--------------------------------------------------------------------')
+let arrayOfObjects = [
+    { a: 1, b: 2 },
+    { a: 1, b: 2 },
+    { c: 3, d: 4 },
+    { a: 5, b: 6 },
+    { e: 7, f: 8 },
+    { g: 9, h: 0 }
+  ];
+  
+  const filterByPropertyA = (obj) => {
+    if (!obj || typeof obj !== "object") throw new Error("pass an object as parameter")
+    return Object.keys(obj).includes("a") ? obj : undefined
+  }
+
+  console.log(customFilterUnique(arrayOfObjects, filterByPropertyA))
